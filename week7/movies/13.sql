f9968c65061999169c54a3c5ac5eff3babc43000
Mytakeon <arthur.alglave@gmail.com>
2020-07-04 00:59:01 +0200

    Improve query 13

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,11 +1,13 @@
-SELECT DISTINCT people.name
-FROM movies
-JOIN stars ON stars.movie_id=movie_id
-JOIN people ON people.id=stars.person_id
-WHERE people.name != "Kevin Bacon"  -- Don't include Kevin in the list
-AND 
-stars.movie_id IN (SELECT movies.id  -- List of the movies Kevin stared in
+WITH joined AS (SELECT stars.movie_id, people.name, people.birth
 	FROM movies
 	JOIN stars ON stars.movie_id=movies.id
-	JOIN people ON people.id=stars.person_id
-	WHERE people.name= "Kevin Bacon" AND people.birth=1958)
+	JOIN people ON people.id=stars.person_id),
+
+movies_with_kevin AS (SELECT movie_id
+	FROM joined
+	WHERE name= "Kevin Bacon" AND birth=1958)
+    
+SELECT DISTINCT name
+FROM joined
+WHERE name != "Kevin Bacon"  -- Don't include Kevin in the list
+AND movie_id IN movies_with_kevin
