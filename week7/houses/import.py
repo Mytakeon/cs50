de3e2bd7ee3ae04aa34d1d51e3a6c942ef07d74a
Mytakeon <arthur.alglave@gmail.com>
2020-07-04 12:15:39 +0200

    Solve houses

--- week7/houses/import.py
+++ week7/houses/import.py
@@ -1,1 +1,17 @@
-# TODO
+import sys
+import cs50
+
+if len(sys.argv) != 2:
+    print("Wrong number of arguments. Usage: python import.py characters.csv")
+    sys.exit(1)
+
+db = cs50.SQL("sqlite:///students.db")
+
+with open(sys.argv[1], 'r') as file:
+    students = [line.strip().split(',') for line in file.readlines()][1:]
+
+for student in students:
+    names = student[0].split(" ")
+    middle = None if len(names) == 2 else names[1]
+    db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
+               names[0], middle, names[-1], student[1], student[2])
--- week7/houses/roster.py
+++ week7/houses/roster.py
@@ -1,1 +1,16 @@
-# TODO
+import sys
+import cs50
+
+if len(sys.argv) != 2:
+    print("Wrong number of arguments. Usage: python roster.py <house_name>")
+    sys.exit(1)
+
+students_db = cs50.SQL("sqlite:///students.db")
+students = students_db.execute("select * from students WHERE house=? ORDER BY last, first", sys.argv[1])
+
+for student in students:
+    _, first, middle, last, house, birth = student.values()
+    if middle:
+        print(f"{first} {middle} {last}, born {birth}")
+    else:
+        print(f"{first} {last}, born {birth}")
